52d602f378236eea66c9fee20b6b94ceb9df0360
Mustafa Aamir Khan <91662007+Mustafakhn@users.noreply.github.com>
2023-01-11 16:30:56 +0530

    updated the logic

--- semver.sh
+++ semver.sh
@@ -1,22 +1,38 @@
 #!/bin/bash
 
+update="$2"
 version="$1"
-major=$(echo "$version" | cut -c1-1)
-minor=$(echo "$version" | cut -c3-3)
-patch=$(echo "$version" | cut -c5-5)
+major=$(echo "$version" | cut -d '-' -f 1)
+minor=$(echo "$version" | cut -d '-' -f 2)
+patch=$(echo "$version" | cut -d '-' -f 3)
 
 
-if [ "$2" == "major" ]; 
+if [ "$update" == "major" ]; 
 then
     major=$(($major + 1))
-elif [ "$2" == "minor" ]; 
+    minor=$(($minor * 0))
+    patch=$(($patch * 0))
+elif [ "$update" == "minor" ]; 
 then
     minor=$(($minor + 1))
-elif  [ "$2" == "patch" ];
+    patch=$(($patch * 0))
+elif  [ "$update" == "patch" ];
 then   
     patch=$(($patch + 1))
 else
     echo "Please provide a version to update"
+fi
+
+if (( "$patch" > "10" ));
+then
+    minor=$(($minor + 1))
+    patch=$(($patch * 0))
+fi
+if (( "$minor" > "10" ));
+then
+    major=$(($major + 1))
+    minor=$(($minor * 0))
+    patch=$(($patch * 0))
 fi
 
 version=$(echo $major-$minor-$patch)
