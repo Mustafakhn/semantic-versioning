a194c670d3db1aba9ce28954835570f14b3618c3
Mustafa Aamir Khan <91662007+Mustafakhn@users.noreply.github.com>
2023-01-11 16:59:00 +0530

    Update semver.sh

--- semver.sh
+++ semver.sh
@@ -7,34 +7,49 @@ minor=$(echo "$version" | cut -d '-' -f 2)
 patch=$(echo "$version" | cut -d '-' -f 3)
 
 
-if [ "$update" == "major" ]; 
+if [[ "$@" == *"help"* ]];
 then
-    major=$(($major + 1))
-    minor=$(($minor * 0))
-    patch=$(($patch * 0))
-elif [ "$update" == "minor" ]; 
-then
-    minor=$(($minor + 1))
-    patch=$(($patch * 0))
-elif  [ "$update" == "patch" ];
-then   
-    patch=$(($patch + 1))
+    echo "
+The correct arguments are the <version.number> and <major>, <minor>, <patch>
+
+major : for updating the major version
+
+minor : for updating the minor version
+
+patch : for updating the patch version
+
+-help or --help : for viewing the help document for the file
+
+which are passed in like ./semver.sh <version.number> <update.type>
+"
 else
-    echo "Please provide a version to update"
-fi
+    if [ "$update" == "major" ]; 
+    then
+        major=$(($major + 1))
+        minor=$(($minor * 0))
+        patch=$(($patch * 0))
+    elif [ "$update" == "minor" ]; 
+    then
+        minor=$(($minor + 1))
+        patch=$(($patch * 0))
+    elif  [ "$update" == "patch" ];
+    then   
+        patch=$(($patch + 1))
+    else
+        echo "Please provide a version to update"
+    fi
+    if (( "$patch" > "9" ));
+    then
+        minor=$(($minor + 1))
+        patch=$(($patch * 0))
+    fi
+    if (( "$minor" > "9" ));
+    then
+        major=$(($major + 1))
+        minor=$(($minor * 0))
+        patch=$(($patch * 0))
+    fi
+    version=$(echo $major-$minor-$patch)
 
-if (( "$patch" > "10" ));
-then
-    minor=$(($minor + 1))
-    patch=$(($patch * 0))
+    echo "version=$version"
 fi
-if (( "$minor" > "10" ));
-then
-    major=$(($major + 1))
-    minor=$(($minor * 0))
-    patch=$(($patch * 0))
-fi
-
-version=$(echo $major-$minor-$patch)
-
-echo "version=$version"
